5a0867eba9f639139fcc2b8f010e647746b7497d
Belal Waw <belalwaw@Belals-MacBook-Pro.local>
2025-05-25 18:57:24 +0100

    updated testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
-from app import app  # Import directly from app.py
+import os
+import sys
 
-def test_index():
+# Add parent directory to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import create_app
+
+def test_home_route():
+    app = create_app({'TESTING': True})
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
